3258ffdc5c420739f281228125a9533c3a7a32ba
Omayma <okhelfaoui23@gmail.com>
2026-09-16 18:39:43 +0100

    Clean up config.php to prevent header warnings

--- config.php
+++ config.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * ================================================================
- *  DATABASE CONFIGURATION — Direct Aiven Production Settings
- * ================================================================
- */
-
 define('DB_HOST', 'mysql-393297ae-okhelfaoui23-4e85.f.aivencloud.com');
 define('DB_PORT', '24322');
 define('DB_NAME', 'defaultdb');
